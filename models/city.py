7d920e30dfa29383e8d2ef6ac169c3d99dd79f16
CodeRaiden <hadoken10@yahoo.com>
2022-12-10 04:29:57 -0800

    fix

--- models/city.py
+++ models/city.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy import Column, String, ForeignKey
+'''
+    Define the class City.
+'''
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
-
-from models.base_model import Base, BaseModel
+import os
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-        places: relationship between a city and places
-    """
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
     __tablename__ = 'cities'
-
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-    places = relationship(
-        'Place', backref='cities', cascade='all, delete-orphan'
-    )
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", passive_deletes=True, backref="cities")
+    else:
+        state_id = ""
+        name = ""
