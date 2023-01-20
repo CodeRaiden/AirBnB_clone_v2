f5a96d16916604003b55a317d3b5ffa65c069acd
CodeRaiden <hadoken10@yahoo.com>
2023-01-20 06:25:43 -0800

    add fixes

--- models/state.py
+++ models/state.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-'''
-    Implementation of the State class
-'''
-
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
+"""This is the state class"""
 import os
 import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    '''
-        Implementation for the State.
-    '''
+    """This is the class for State
+    Attributes:
+        name: input name
+        cities = relationship between state and city tables.
+    """
+
     __tablename__ = 'states'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", passive_deletes=True, backref="state")
+        cities = relationship(
+            'City', back_populates='state',
+            cascade='all, delete, delete-orphan')
+
     else:
         name = ""
 
         @property
         def cities(self):
-            """
-            cities property
-            """
-            city_list = []
-            for key, val in models.storage.all().items():
-                try:
-                    if val.state_id == self.id:
-                        city_list.append(val)
-                except AttributeError:
-                    pass
-            return city_list
+            """returns list of Cities and some relationships"""
+            cities_instances = []
+            cities_dict = models.storage.all(models.City)
+            for key, value in cities_dict.items():
+                if self.id == value.state_id:
+                    cities_instances.append(value)
+            return cities_instances
